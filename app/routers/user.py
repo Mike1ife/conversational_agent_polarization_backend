931bd0e7a133a53ba96fdb8ef199130891c5e437
Mike1ife <yitai77@gmail.com>
2026-02-24 17:29:40 -0500

    Refactor user state routes to include error handling for invalid study IDs

--- app/routers/user.py
+++ app/routers/user.py
@@ -19,11 +19,17 @@ def validate_study_id(study_id: str):
 
 @router.get("/state/{study_id}")
 def get_user_state_route(study_id: str):
-    curr_state = get_user_state(study_id=study_id)
-    return curr_state.model_dump(by_alias=True)
+    if study_id_is_valid(study_id=study_id):
+        curr_state = get_user_state(study_id=study_id)
+        return curr_state.model_dump(by_alias=True)
+    else:
+        raise HTTPException(status_code=404, detail="Study ID Not Found")
 
 
 @router.post("/advance/{study_id}")
 def advance_user_state_route(study_id: str, next_state: UserState):
-    advance_user_state(study_id=study_id, next_state=next_state)
-    return {"message": "Advance User State Successfully"}
+    if study_id_is_valid(study_id=study_id):
+        advance_user_state(study_id=study_id, next_state=next_state)
+        return {"message": "Advance User State Successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Study ID Not Found")
